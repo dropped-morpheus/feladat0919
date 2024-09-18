1ccc75332a72b98cb12eeeb4a1c250b1498f56ef
Beliczky Zsolt <beliczkyzsolt@gmail.com>
2024-09-18 21:29:47 +0200

    Init commit2

---
+++ feladat0919.js
@@ -0,0 +1,15 @@
+/*
+Vizsgáljátok meg hogy a bemenetek valóban számok-e (a típusa szám-e).
+1-4: Írjatok js függvényeket két szám összeadására, kivonására, szorzásra, osztásra.
+5: Írjatok hatványozásra saját implementációt (kettő a harmadikon egyenlő nyolc).
+6: Írjatok függvényt egy 0 és 1ből álló string beolvasására. A bináris kódot alakítsátok számmá (Saját implementáció).
+*/
+
+
+
+console.log(customAdd(5,2))
+console.log(customSubtract(5,2))
+console.log(customMultiply(5,2))
+console.log(customDivision(5,2))
+console.log(customPow(5,2))
+console.log(customBinToDec("011101"))
